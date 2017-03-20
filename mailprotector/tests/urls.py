cdcc21064634189441aaa71bccca51af81cf489a
benzkji <bnzk@bnzk.ch>
2017-03-21 00:57:38 +0100

    added textblock protection

--- mailprotector/tests/urls.py
+++ mailprotector/tests/urls.py
@@ -1,11 +1,11 @@
 """URLs to run the tests."""
-from compat import patterns, include, url
+# from compat import patterns, include, url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
-admin.autodiscover()
-
-urlpatterns = patterns(
-    '',
+urlpatterns = [
+    url(r'^test/$', TemplateView.as_view(template_name='test_app/test.html'), name='test_tag_view'),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
