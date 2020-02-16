54338f171dd56ff4851a29175b22ff26028c587c
Ben Stähli <bnzk@bnzk.ch>
2020-02-16 14:35:21 +0100

    coverage

--- mailprotector/conf.py
+++ mailprotector/conf.py
@@ -12,8 +12,7 @@ phone_link_pattern = r'<a[^>]*href=("|\')?tel:(' + phone_pattern + ')[^>]*>([^<]
 
 
 class MailprotectorConf(AppConf):
-    # MODE = 'munger'
-    MODE = 'munger_link_only'
+    MODE = 'munger'
     EMAIL_PATTERN = re.compile(r'(' + email_pattern + r')')
     EMAIL_LINK_PATTERN = re.compile(email_link_pattern)
     PHONE_PATTERN = re.compile(r'(' + phone_pattern + r')')
