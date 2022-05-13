ae31103136d3e885d941702b1b3543c0215ccb4c
Ben Stähli <bnzk@bnzk.ch>
2022-05-13 10:12:47 +0200

    fix django 4.0 compat

--- mailprotector/tests/urls.py
+++ mailprotector/tests/urls.py
@@ -1,11 +1,11 @@
 """URLs to run the tests."""
 # from compat import patterns, include, url
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^test/$', TemplateView.as_view(template_name='test_app/test.html'), name='test_tag_view'),
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^test/$', TemplateView.as_view(template_name='test_app/test.html'), name='test_tag_view'),
+    re_path(r'^admin/', admin.site.urls),
 ]
